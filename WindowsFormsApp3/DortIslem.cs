e21e520216e86a08f8c9217810a0003ec193675c
Nihat <Nihat@DESKTOP-BGVISC5>
2023-07-26 14:05:14 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApp3/DortIslem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    internal class DortIslem
+    {
+        public void topla(int sayi1,int sayi2)
+        {
+            int sonuc = sayi1 + sayi2;
+            MessageBox.Show(sonuc.ToString());
+        }
+    }
+}
